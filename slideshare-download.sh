90cd7aefbd68c61e0a304ee67faa29e6d944ccf2
shgtkshruch <shgtk.shruch@gmail.com>
2014-03-15 12:00:18 +0900

    Upgrade slideshare-download.sh

--- slideshare-download.sh
+++ slideshare-download.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
+#
+# Download slide and create pdf and note
 
-# get_slide
-cd
+# Variables
 echo "Slideshare URL:"
 read URL
 DATE=$(date +%Y%m%d)
 SLIDE_TITLE=`curl "${URL}" | grep \<title\> | gsed 's/\  <title>//' | gsed 's/<\/title>//'`
-python ~/bin/slideshare-download.py "${URL}"
-wait
-
-# rename_folder
 FOLDER_NAME="${DATE} ${SLIDE_TITLE}"
-mv ~/slide ~/"${FOLDER_NAME}"
+NOTEBOOK="0302 schoo"
 
-# image_to_pdf
-cd ~/"${FOLDER_NAME}"
-rename -s - -0 slide-[1-9]-1024.jpg
-convert *.jpg "${SLIDE_TITLE}".pdf
-wait
-rm *.jpg
+# functions
+get_slide(){
+  python ~/bin/slideshare-download.py "${URL}"
 
-# move_folder
+  # rename_folder
+  mv $HOME/slide $HOME/"${FOLDER_NAME}"
+
+  # rename slide image files
+  cd $HOME/"${FOLDER_NAME}"
+  rename -s - -0 slide-[1-9]-1024.jpg
+
+  # sum image size
+  IMAGE_SIZE=`ls -lh | awk '{print $5}' | gsed 's/K//g' | awk '{sum+=$1} END{print sum}'`
+}
+
+image_to_pdf(){
+  convert *.jpg "${SLIDE_TITLE}".pdf
+  wait
+  rm *.jpg
+}
+
+move_folder(){
+  mv $HOME/"${FOLDER_NAME}" $HOME/Documents/schoo
+}
+
+create_note(){
+  export NOTE_TITILE="${SLIDE_TITLE}"
+  geeknote create --title "${NOTE_TITILE}" --content " " --notebook "${NOTEBOOK}" --tags "slide"
+}
+
+# start
 cd
-mv "${FOLDER_NAME}" ~/Documents/schoo
 
-# geeknote
-export NOTE_TITILE="${SLIDE_TITLE}"
-geeknote create --title "${NOTE_TITILE}" --content " " --notebook "0302 schoo" --tags "slide"
+get_slide
+wait
 
+move_folder
+
+if [ "${IMAGE_SIZE}" -gt 8000 ]; then
+  open -a Preview.app $HOME/Documents/schoo/"${FOLDER_NAME}"/*.jpg
+  echo "${SLIDE_TITLE}" | pbcopy
+else
+  cd $HOME/Documents/schoo/"${FOLDER_NAME}"
+  image_to_pdf
+fi
+
+create_note
